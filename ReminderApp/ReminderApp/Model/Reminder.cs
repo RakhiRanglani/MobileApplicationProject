2b758529173b074d2d61b7c3ce4ad02b87a0a9c4
Rakhi Ranglani <rakhi.ranglani222@outlook.com>
2021-05-03 01:13:12 +0100

    Added Expirydate column

--- ReminderApp/ReminderApp/Model/Reminder.cs
+++ ReminderApp/ReminderApp/Model/Reminder.cs
@@ -11,5 +11,7 @@ namespace ReminderApp.Model
         public int ID { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public string ExpiryDate { get; set; }
     }
 }
